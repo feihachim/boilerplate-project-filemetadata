dc33f590b9ad1b1d997081b010946b405681cbe5
Hachim YOUSSOUFA <feihachim@yahoo.fr>
2023-01-18 10:25:02 +0100

    almost done?

--- index.js
+++ index.js
@@ -1,17 +1,42 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
+const multer=require('multer');
 
 var app = express();
 
+const storage=multer.diskStorage({
+  destination:function(req,file,cb){
+    cb(null,'uploads');
+  },
+  filename:function(req,file,cb){
+    cb(null,file.fieldname+'-'+Date.now());
+  }
+});
+const upload=multer({ storage:storage });
+
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
 
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
+app.post('/api/fileanalyse', upload.single('upfile') ,(req,res,next)=>{
+  const file=req.file;
+  if(!file){
+    const error=new Error('Please upload a file');
+    error.httpStatusCode=400;
+    return next(error);
+  }
+  res.json({
+    name:file.originalname,
+    type:file.mimetype,
+    size:file.size
+  });
+});
 
 
 const port = process.env.PORT || 3000;
